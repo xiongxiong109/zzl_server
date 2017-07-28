260014432f9541fa992dd753620e76ed2ca3e594
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-07-28 16:32:34 +0800

    service

---
+++ routes/mockApis.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var Mock = require('mockjs');
+// 使用mock模块生成对应契约的mock数据
+
+/* mock apis */
+router.get('/search', function(req, res, next) {
+	let data = Mock.mock({
+		'list|3-10': [{
+			'id|+1': 1,
+			'starttime': '@DATE(MM-dd)',
+			'endtime': '@DATE(MM-dd)',
+			'str': '@FIRST'
+		}]
+	});
+	setTimeout(() => {
+		res.send(data);
+	}, Math.random() * 300 + 300);
+});
+
+module.exports = router;
