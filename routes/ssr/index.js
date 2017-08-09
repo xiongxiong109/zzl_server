06f2a31bf4861ad1430e3c8bf650b0da5b548d51
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-08-09 13:06:48 +0800

    update ssr

--- routes/ssr/index.js
+++ routes/ssr/index.js
@@ -7,6 +7,17 @@ const App = new Vue({
 			msg: 'hello vue ssr'
 		}
 	},
+	beforeCreate() {
+		console.log('before create')
+	},
+	created() {
+		console.log('created')
+	},
+	// ssr中, 组件的生命周期只有beforeCreate和created可以被触发
+	// 而且触发过程是在node端
+	// mounted() {
+	// 	console.log('mounted')
+	// },
 	template: '<div class="box">{{msg}}</div>'
 })
 
